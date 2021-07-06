5e32c3c677c089a5551180668221a5a6d75bf70d
reactnodejs32 <reactnodejs32@gmail.com>
2021-07-06 15:17:24 -0400

    checking once more

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-console.log(process.env.TEST);
+console.log(process.env.TEST_VALUE);
 
-if (process.env.test === "TEST_VALUE") {
+if (process.env.TEST_VALUE === "TEST_VALUE") {
   console.log("TEST_VALUE");
 } else {
   console.log("IF STATE IS NOT TRUE");
