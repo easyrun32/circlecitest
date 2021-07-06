a0684679fd6f2de0f1bcf3e65a235b19337cea70
reactnodejs32 <reactnodejs32@gmail.com>
2021-07-06 15:18:28 -0400

    hmmm

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log(process.env.TEST_VALUE);
 
 if (process.env.TEST_VALUE === "TEST_VALUE") {
-  console.log("TEST_VALUE");
+  console.log("WE GOT ! TEST_VALUE");
 } else {
   console.log("IF STATE IS NOT TRUE");
 }
